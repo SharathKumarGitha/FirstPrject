cd74e63be4ddcda2801da46812e2c67969f12c97
SharathKumarGitha <97340761+SharathKumarGitha@users.noreply.github.com>
2022-04-28 21:20:04 +0530

    Update SampleClass.java

--- src/main/java/com/one/FirstProject/SampleClass.java
+++ src/main/java/com/one/FirstProject/SampleClass.java
@@ -4,7 +4,7 @@ public class SampleClass {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Hi this is sharath......");
+		System.out.println("Hi this is sharath kumar....................");
 	}
 
 }
